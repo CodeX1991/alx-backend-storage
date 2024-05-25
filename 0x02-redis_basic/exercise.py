c7b1b358441c55c8cc0128bee1f3095be6e9267a
CodeX1991 <oghenewonaholy@gmail.com>
2024-05-25 13:12:01 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,7 +15,7 @@ class Cache:
     def __init__(self):
         """Initializing class variable"""
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Return data in string format"""
